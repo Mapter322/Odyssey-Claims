014ef1af98db42849f62cee74aab0554ca1cf00a
mapter <mapter0k@gmail.com>
2026-09-14 17:27:25 +0500

    generate claim setting defaults in config

--- common/src/main/java/earth/terrarium/cadmus/common/config/CadmusConfig.java
+++ common/src/main/java/earth/terrarium/cadmus/common/config/CadmusConfig.java
@@ -5,6 +5,10 @@ import com.google.gson.GsonBuilder;
 import com.teamresourceful.resourcefullib.common.exceptions.NotImplementedException;
 import dev.architectury.injectables.annotations.ExpectPlatform;
 import earth.terrarium.cadmus.Cadmus;
+import earth.terrarium.cadmus.api.settings.SettingScope;
+import earth.terrarium.cadmus.api.settings.SettingTarget;
+import earth.terrarium.cadmus.api.settings.types.BooleanSetting;
+import earth.terrarium.cadmus.common.settings.SettingDefinitions;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -23,7 +27,18 @@ public class CadmusConfig {
     public int defaultMaxChunkLoadedClaims = 64;
     public int minChunksBetweenTowns = 10;
     public int personalCampDurationSeconds = 300;
-    public Map<String, Boolean> defaultClaimSettings = new LinkedHashMap<>();
+    public Map<String, Boolean> defaultClaimSettings = createDefaultClaimSettings();
+
+    private static Map<String, Boolean> createDefaultClaimSettings() {
+        Map<String, Boolean> defaults = new LinkedHashMap<>();
+        SettingDefinitions.forScope(SettingScope.TOWN).forEach((id, definition) -> {
+            if (definition.target() != SettingTarget.GLOBAL) return;
+            if (definition.defaultValue() instanceof BooleanSetting setting) {
+                defaults.put(id, setting.value());
+            }
+        });
+        return defaults;
+    }
 
     public static CadmusConfig get() {
         if (INSTANCE == null) {
@@ -47,6 +62,10 @@ public class CadmusConfig {
             try (Reader reader = Files.newBufferedReader(path)) {
                 CadmusConfig config = GSON.fromJson(reader, CadmusConfig.class);
                 if (config != null) {
+                    if (config.defaultClaimSettings == null || config.defaultClaimSettings.isEmpty()) {
+                        config.defaultClaimSettings = createDefaultClaimSettings();
+                    }
+                    config.save();
                     return config;
                 }
             } catch (IOException | RuntimeException e) {
